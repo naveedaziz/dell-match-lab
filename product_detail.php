bdfae7ef33894a53cefbb9168db0d05b6bd62922
naveedaziz <naveed.aziz@bramerz.pk>
2013-11-11 15:59:53 +0500

    OG

--- product_detail.php
+++ product_detail.php
@@ -27,7 +27,7 @@ foreach($productInfo as $index=>$val){
   <!-- FB Meta Tags -->
 <meta property="fb:app_id" content="365073203627947" />
 <meta property="og:title" content="<?=$name;?>" />
-<meta property="og:type" content="Dell Match Lab:product" />
+
 <meta property="og:url" content="http://dellmatchlab.com/product_detail?id=<?=$id;?>" />
 <meta property="og:image" content="<?=$image;?>" />
 <meta property="og:description" content="<?=$shortDesp;?>" />
